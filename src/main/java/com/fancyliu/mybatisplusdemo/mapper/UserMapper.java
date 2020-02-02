a4302bde220c7822065ede94a37815be3c3d26cb
liufan <flanliulf@gmail.com>
2020-02-02 10:28:40 +0800

    MyBatis Plus 代码生成器示例

--- src/main/java/com/fancyliu/mybatisplusdemo/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.fancyliu.mybatisplusdemo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.fancyliu.mybatisplusdemo.entity.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
